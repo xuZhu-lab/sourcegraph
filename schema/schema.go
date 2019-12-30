735519abe43611f6f4062de4ebee905142ec3d39
Quinn Slack <qslack@qslack.com>
2019-12-29 21:45:30 -0800

    wip

--- schema/schema.go
+++ schema/schema.go
@@ -808,6 +808,8 @@ type Settings struct {
 
 // SettingsExperimentalFeatures description: Experimental features to enable or disable. Features that are now enabled by default are marked as deprecated.
 type SettingsExperimentalFeatures struct {
+	// HomeSections description: Enables collapsible sections on the homepage.
+	HomeSections *bool `json:"homeSections,omitempty"`
 	// SearchStats description: Enables a new page that shows language statistics about the results for a search query.
 	SearchStats *bool `json:"searchStats,omitempty"`
 	// SplitSearchModes description: Enables toggling between the current omni search mode, and experimental interactive search mode.
